38ee7e30895a15e4b45b0aa3ac46053194d0c03c
AE-ALEmran <alemranak13@gmail.com>
2024-01-07 06:26:24 +0600

    class 12

---
+++ bonus code/bonus 12.py
@@ -0,0 +1,10 @@
+feet_inches = input("Enter the feet and inches :")
+def convert(feet_inches):
+    parts = feet_inches.split(" ")
+    feet = float(parts[0])
+    inches = float(parts[1])
+
+    meters = feet * 0.3048 + inches * 0.0254
+    return f"{feet} feets and {inches} inches is equal to {meters} meters."
+
+print(convert(feet_inches))
---
+++ day1main/todo list class 12.py
@@ -0,0 +1,73 @@
+def get_store(filepath):
+    with open(filepath, 'r') as file_local:
+        store_local = file_local.readlines()
+    return store_local
+
+#use def function to define repeted code in the program.
+def write_store(filepath, store_arg):
+    with open(filepath, 'w') as file:
+        file.writelines(store_arg)
+
+
+while True:
+    user_choice = input("Enter show, add , Edit, exit, complete code :")
+    user_choice = user_choice.strip()
+
+    if user_choice.startswith('add'):
+        todo = user_choice[4:]
+
+        store = get_store(".idea/store.txt")                           # replace function
+
+        store.append(todo + '\n')
+
+        write_store(".idea/store.txt", store)
+    elif user_choice.startswith('show'):
+
+        store = get_store(".idea/store.txt")
+
+        new_store=[item.strip('\n') for item in store]
+
+        for index , item in enumerate(new_store):
+            item=item.title()
+            index=index+1
+            row=f"{index}.{item}"
+            print(row)
+    elif user_choice.startswith('edit'):
+        try :
+            number = int(user_choice[5:])
+            print(number)
+            number=number-1
+
+            store = get_store(".idea/store.txt")
+
+            new_todo=input("Enter the new Todo :")
+            store[number]=new_todo + '\n'
+
+            write_store(".idea/store.txt", store)
+        except ValueError :
+            print("Your comment is not valid.")
+            continue
+
+    elif user_choice.startswith('complete') :
+        try :
+            number = int(user_choice[9:])
+            store = get_store(".idea/store.txt")
+            index=number-1
+            todo_to_remove=store[index].strip('\n')
+            store.pop(index)                                       # here strip() use for remove line gap
+
+            write_store(".idea/store.txt", store)
+
+            massage= f"Todo {todo_to_remove} was remove from the list !!!!!!"
+            print(massage)
+        except IndexError :
+            print("Your enter number is out of the range .")
+            continue
+
+    elif '_' in user_choice:
+        print("Hey, Eneter the write variables!!!")
+    elif user_choice.startswith('exit') :
+        break
+    else:
+        print("Commend is not valid")
+
