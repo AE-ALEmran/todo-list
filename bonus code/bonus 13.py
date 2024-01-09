7e6decd97e43260eb19dc2533532605f37a7b956
AE-ALEmran <alemranak13@gmail.com>
2024-01-09 11:06:21 +0600

    class 13

--- bonus code/bonus 13.py
+++ bonus code/bonus 13.py
@@ -1,16 +1,21 @@
 feet_inches = input("Enter the feet and inches :")
 
-def convert(feet_inches):
+def pers(feet_inches) :
     parts = feet_inches.split(" ")
     feet = float(parts[0])
     inches = float(parts[1])
+    return {"feet": feet, "inches": inches}
 
+
+def convert(feet, inches):
     meters = feet * 0.3048 + inches * 0.0254
     return meters
+parsed=pers(feet_inches)
+result = convert(parsed['feet'], parsed['inches'])
 
-result = convert(feet_inches)
+print(f"{parsed['feet']} Feet and {parsed['inches']} Inches are convert to meters {result}")
 
-if result < 1 :
+if result < 1:
     print("The kid is too small.")
 else:
     print("The kid can use the slide.")
