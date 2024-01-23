0745ef8f4e8a9495da2005832b5f005143b8ec2c
AE AL Emran <alemranak13@gmail.com>
2024-01-23 06:44:34 +0600

    Implement add and break button

--- day1main/gui.py
+++ day1main/gui.py
@@ -2,9 +2,24 @@ import functions
 import PySimpleGUI
 
 label = PySimpleGUI.Text("Type of To_Do")
-input_box = PySimpleGUI.InputText(tooltip="Enter To Do")
+input_box = PySimpleGUI.InputText(tooltip="Enter To Do", key= "todo")
 add_button = PySimpleGUI.Button("Add")
 
-window=PySimpleGUI.Window('My To-Do List', layout=[[[label], [input_box, add_button]]])
-window.read()
+window=PySimpleGUI.Window('My To-Do List',
+                          layout = [[[label], [input_box, add_button]]],
+                          font=('Helvertica', 15))
+
+while True:
+    event, values = window.read()
+    print(event)
+    print(values)
+    match event :
+        case 'Add' :
+            todos = functions.get_store()
+            new_todos = values['todo'] + '\n'
+            todos.append(new_todos)
+            functions.write_store(todos)
+        case PySimpleGUI.WINDOW_CLOSED :
+            break
+
 window.close()
